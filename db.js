6a314c2b861147bb992e2e08c26974583e586838
gayathri-ambati <gayathriambati444@gmail.com>
2026-07-01 13:39:01 +0530

    Initial commit

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '', // Handle empty password
-    database: process.env.DB_NAME || 'epitome_steel',
+    database: process.env.DB_NAME || 'Columbia',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
